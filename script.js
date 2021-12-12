076d9b197ce0a7592ef35cf0f10a85c189cdfd4c
Chloe <glynnchloe93@gmail.com>
2021-12-12 22:47:00 +0000

    third commit

--- script.js
+++ script.js
@@ -2,6 +2,122 @@
 function ageprompt(){
   var r = confirm("Are you over 18?");
   if (r == false){
-    alert("Sorry, you must be over 18 to access this page!")
+    alert("Sorry, you must be over 18 to access this page!");
+    window.open('index.html');
   }
+  else{
+    window.open('nightlife/index.html');
+  }
+}
+
+function clickWigwamWebsite(){
+  window.open('https://wigwamdublin.com/','_blank');
+}
+
+function clickWigwamLocation(){
+  window.open('https://www.google.com/maps/place/Wigwam/@53.3478921,-6.264497,17z/data=!3m1!4b1!4m5!3m4!1s0x48670e8378b543e3:0x4ae76b7fd4a3378c!8m2!3d53.3478889!4d-6.2623083', '_blank');
+}
+
+function clickBahWebsite(){
+  window.open('https://bah33.ie/','_blank');
+}
+
+function clickBahLocation(){
+  window.open('https://www.google.com/maps/place/BAH33%C2%B0/@53.3414669,-6.2609733,17z/data=!3m1!4b1!4m5!3m4!1s0x48670e9beacfb91d:0x4c9610f6602ea00!8m2!3d53.3414637!4d-6.2587846','_blank');
+}
+
+function clickWasabiWebsite(){
+  window.open('https://www.wasabi.ie/','_blank');
+}
+
+function clickWasabiLocation(){
+  window.open('https://www.google.com/maps/place/Wasabi+Bar+%26+Grill/@53.3576154,-6.2653802,17z/data=!3m2!4b1!5s0x48670e7ddd1ca50b:0x67a363768547c189!4m5!3m4!1s0x48670fee872eff57:0x9efea168cecc1c41!8m2!3d53.3576122!4d-6.2631915','_blank');
+}
+
+function clickBrasileiroWebsite(){
+  window.open('https://www.instagram.com/obrasileiroshopoficial/?hl=en','_blank');
+}
+
+function clickBrasileiroLocation(){
+  window.open('https://www.google.com/maps/place/O+Brasileiro+Shop/@53.3495443,-6.2712204,17z/data=!3m1!4b1!4m5!3m4!1s0x48670e8209090819:0x39611383a4b4f33d!8m2!3d53.3495405!4d-6.2690381','_blank');
+}
+
+function clickChurrosWebsite(){
+  window.open('https://www.instagram.com/churrosdolulu/?hl=en','_blank');
+}
+
+function clickChurrosLocation(){
+  window.open('https://www.google.com/maps/place/Churros+do+Lulu/@53.3501588,-6.2707787,17z/data=!3m1!4b1!4m5!3m4!1s0x48670f787b12c3d9:0xeb7a95831d675781!8m2!3d53.3501672!4d-6.2685808','_blank');
+}
+
+function clickPastelWebsite(){
+  window.open('https://www.instagram.com/pastelkingdublin/?hl=en','_blank');
+}
+
+function clickPastelLocation(){
+  window.open('https://www.google.com/maps/place/Pastel+King/@53.3508319,-6.2649967,17z/data=!3m1!4b1!4m5!3m4!1s0x48670fbe177e019d:0x3ed9b8efa59d79!8m2!3d53.3508287!4d-6.262808','_blank');
+}
+
+function clickDiceysWebsite(){
+  window.open('https://www.instagram.com/diceysgardenbar/?hl=en','_blank');
+}
+
+function clickDiceysLocation(){
+  window.open("https://www.google.com/maps/place/Dicey's+Garden+Club/@53.3358671,-6.2657456,17z/data=!3m1!4b1!4m5!3m4!1s0x48670e9fbd59debb:0x48ff9c9781cd8e3d!8m2!3d53.3358504!4d-6.2635152",'_blank');
+}
+
+function clickTramlineWebsite(){
+  window.open('https://tramline.ie/','_blank');
+}
+
+function clickTramlineLocation(){
+  window.open("https://www.google.com/maps/place/TRAMLINE/@53.3464508,-6.2600526,17z/data=!3m1!4b1!4m5!3m4!1s0x48670e8531d75551:0x82263e68ca6bf87c!8m2!3d53.3464476!4d-6.2578639",'_blank');
+}
+
+function clickCapoeiraWebsite(){
+  window.open("https://www.dublin-capoeira.ie/",'_blank');
+}
+
+function clickCapoeiraLocation(){
+  window.open("https://www.google.com/maps/place/DUBLIN+CAPOEIRA+PROFESSOR+BRUNO/@53.3594112,-6.3015299,14z/data=!4m5!3m4!1s0x48670fb7e64f9863:0xb55f10b8ebc61fb9!8m2!3d53.3479111!4d-6.2612862",'_blank');
+}
+
+function clickJSBJJWebsite(){
+  window.open("https://www.jsbjj.ie/",'_blank');
+}
+
+function clickJSBJJLocation(){
+  window.open("https://www.google.com/maps/place/JS+Brazilian+Jiu+Jitsu+Academy/@53.3508539,-6.2677305,17z/data=!3m1!4b1!4m5!3m4!1s0x48670e817effe4c9:0xeb56efb572b2767c!8m2!3d53.3508477!4d-6.2655385",'_blank');
+}
+
+function clickVieiraWebsite(){
+  window.open("https://vieirahighperformance.maonrails.com/",'_blank');
+}
+
+function clickVieiraLocation(){
+  window.open("https://www.google.com/maps/place/Vieira+High+Performance/@53.3594112,-6.3015299,14z/data=!3m1!5s0x48670e848c10735d:0xb70b647c387e2265!4m5!3m4!1s0x48670f5e139dbe2f:0xc71133e5c760807b!8m2!3d53.347872!4d-6.2604885",'_blank');
+}
+
+function clickPerolaWebsite(){
+  window.open("https://perolapadilhaclinicspa.com/?lang=en",'_blank');
+}
+
+function clickPerolaLocation(){
+  window.open("https://www.google.com/maps/place/Perola+Padilha+Clinic+%26+Spa/@53.3456791,-6.2576109,17z/data=!3m1!4b1!4m5!3m4!1s0x48670fb04a7c3095:0xacb3551e8689fecb!8m2!3d53.3456775!4d-6.2552729?hl=en",'_blank');
+}
+
+function clickEssencesWebsite(){
+  window.open("https://www.essencesdayspa.ie/",'_blank');
+}
+
+function clickEssencesLocation(){
+  window.open("https://www.google.com/maps/place/Essences+Day+Spa/@53.3432874,-6.2639741,17z/data=!3m1!4b1!4m5!3m4!1s0x48670f064607fb95:0x3d8e11bdb7ed18e7!8m2!3d53.3432842!4d-6.2617854",'_blank');
+}
+
+function clickBelezaWebsite(){
+  window.open("https://www.instagram.com/belezapbeauty/?hl=en",'_blank');
+}
+
+function clickBelezaLocation(){
+  window.open("https://www.google.com/maps/place/Beleza+Pura+by+Camila+Marc/@53.3525263,-6.2665406,17z/data=!3m1!4b1!4m5!3m4!1s0x48670e8287497abb:0xbc18608258c7fe7f!8m2!3d53.3525491!4d-6.2643025",'_blank');
 }
